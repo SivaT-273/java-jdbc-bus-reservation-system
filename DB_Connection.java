1813b6e69571a97ffcd3f624dc4b7ef77c2da4c0
SivaT <sivakumar020703@gmail.com>
2026-02-14 15:52:05 +0530

    add JDBC database connection handle

--- DB_Connection.java
+++ DB_Connection.java
@@ -5,7 +5,7 @@ public class DB_Connection
 {
 	private static String url      = "jdbc:mysql://localhost:3306/BusReservation";
 	private static String userName = "root";
-	private static String passWord = "MySQL@2003";
+	private static String passWord = "enterpassword";
 	
 	public static Connection getConnection() throws SQLException
 	{
